e7f39997ab2c92c2a63c365bcf8d1549220a8575
Santi García <58360969+SantiTheHuman@users.noreply.github.com>
2022-04-14 23:22:05 +0200

    update packages and fix react-router

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
 import React from "react";
 import { ApolloProvider } from "@apollo/client";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, BrowserRouter, Routes } from "react-router-dom";
 import { graphClient } from "./data/apollo-client";
 import MapContainer from "./components/Map/MapContainer";
 import "./App.css";
 
 function App() {
   return (
-    <Router>
       <ApolloProvider client={graphClient}>
-        <div className="App">
+    <BrowserRouter>
+    <Routes>
           <Route
             exact
             path="/"
-            render={() => (
-              <>
+            element={<div className="App">
                 <h1>LAYERS</h1>
                 <h3>A Collaborative Map</h3>
                 <p>Currently in development.</p>
                 <Link to="/Map">Layers 0.00001</Link>
-              </>
-            )}
+                </div>
+            }
           />
-          <Route exact path="/Map" render={() => <MapContainer />} />
-        </div>
+          <Route exact path="/Map" element={<MapContainer />} />
+</Routes>
+      </BrowserRouter>
       </ApolloProvider>
-    </Router>
   );
 }
 
