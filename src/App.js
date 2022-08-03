f397ad4cfa35f273b4a664184320afc2f9ab9045
Sania García <58360969+saniagarciadev@users.noreply.github.com>
2022-08-03 19:34:01 +0200

    fix react-router-dom issues

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
 import React from "react";
 import { ApolloProvider } from "@apollo/client";
-import { BrowserRouter as Router, Route, Link, BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { graphClient } from "./data/apollo-client";
 import MapContainer from "./components/Map/MapContainer";
 import "./App.css";
 
 function App() {
   return (
-      <ApolloProvider client={graphClient}>
-    <BrowserRouter>
-    <Routes>
+    <ApolloProvider client={graphClient}>
+      <BrowserRouter>
+        <Routes>
           <Route
             exact
             path="/"
-            element={<div className="App">
+            element={
+              <div className="App">
                 <h1>LAYERS</h1>
                 <h3>A Collaborative Map</h3>
                 <p>Currently in development.</p>
                 <Link to="/Map">Layers 0.00001</Link>
-                </div>
+              </div>
             }
           />
           <Route exact path="/Map" element={<MapContainer />} />
-</Routes>
+        </Routes>
       </BrowserRouter>
-      </ApolloProvider>
+    </ApolloProvider>
   );
 }
 
