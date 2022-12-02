54ae9c2a6faaaa9dd10d49c9d840801bce149efb
Sania García <58360969+saniagarciadev@users.noreply.github.com>
2022-12-02 18:35:39 +0100

    remove aws import and datatest

--- src/App.js
+++ src/App.js
@@ -2,13 +2,8 @@ import React from "react";
 import { ApolloProvider } from "@apollo/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { graphClient } from "./data/apollo-client";
-import { Amplify, API, graphqlOperation } from "aws-amplify";
-import awsconfig from "./aws-exports";
 import MapContainer from "./components/Map/MapContainer";
 import "./App.css";
-import DataTest from "./components/Test/DataTest";
-
-Amplify.configure(awsconfig);
 
 function App() {
   return (
@@ -23,13 +18,10 @@ function App() {
                 <h1>LAYERS</h1>
                 <h3>A Collaborative Map</h3>
                 <p>Currently in development.</p>
-                <Link to="/Map">Layers Map 0.00001</Link>
-                <Link to="/data-test">Data test</Link>
               </div>
             }
           />
           <Route exact path="/Map" element={<MapContainer />} />
-          <Route path="/*" element={<DataTest />} />
         </Routes>
       </BrowserRouter>
     </ApolloProvider>
